435a3a755a05cab162fa267ab087b6339c509184
dionomusuko <ruby.school@gmail.com>
2019-12-13 16:42:20 +0900

    change users/show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_064808) do
+ActiveRecord::Schema.define(version: 2019_12_11_102540) do
 
   create_table "controllers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -48,6 +48,9 @@ ActiveRecord::Schema.define(version: 2019_12_11_064808) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
+    t.text "avatar"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
